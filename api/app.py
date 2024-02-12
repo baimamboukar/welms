f03c68aef466d2fd038cd3b47d0e055eb915632b
baimamboukar <baimamboukar@gmail.com>
2024-02-13 00:13:58 +0100

    setup: init welms website

---
+++ api/app.py
@@ -0,0 +1,36 @@
+"""WELMS API BACKEND"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def abcrf():
+    """--Returns index.html--"""
+
+    return render_template('index6.html')
+
+
+@app.route('/about')
+def about():
+    """--Returns About--"""
+    return render_template('about.html')
+
+
+@app.route('/partners')
+def partners():
+    """--Returns About--"""
+    return render_template('blog.html')
+
+
+@app.route('/blog_single')
+def blog_single():
+    """--Returns About--"""
+    return render_template('single-blog.html')
+
+
+@app.route('/single_blog')
+def single_blog():
+    """--Returns About--"""
+    return render_template('single-blog.html')
