92ad06f60d10ca9ef7e72bd21d38459d8a79ae50
baimamboukar <baimamboukar@gmail.com>
2024-02-14 13:05:28 +0100

    chore: add vercel config

--- api/index.py
+++ api/index.py
@@ -12,37 +12,37 @@ def abcrf():
     return render_template('index6.html')
 
 
-# @app.route('/about')
-# def about():
-#     """--Returns About--"""
-#     return render_template('about.html')
+@app.route('/about')
+def about():
+    """--Returns About--"""
+    return render_template('about.html')
 
 
-# @app.route('/partners')
-# def partners():
-#     """--Returns About--"""
-#     return render_template('blog.html')
+@app.route('/partners')
+def partners():
+    """--Returns About--"""
+    return render_template('blog.html')
 
 
-# @app.route('/blog_single')
-# def blog_single():
-#     """--Returns About--"""
-#     return render_template('single-blog.html')
+@app.route('/blog_single')
+def blog_single():
+    """--Returns About--"""
+    return render_template('single-blog.html')
 
 
-# @app.route('/single_blog')
-# def single_blog():
-#     """--Returns About--"""
-#     return render_template('single-blog.html')
+@app.route('/single_blog')
+def single_blog():
+    """--Returns About--"""
+    return render_template('single-blog.html')
 
 
-# @app.route('/team')
-# def team():
-#     """--Returns Team--"""
-#     return render_template('team.html')
+@app.route('/team')
+def team():
+    """--Returns Team--"""
+    return render_template('team.html')
 
 
-# @app.route('/members')
-# def members():
-#     """--Returns Memebexrs--"""
-#     return render_template('team.html')
+@app.route('/members')
+def members():
+    """--Returns Memebexrs--"""
+    return render_template('team.html')
